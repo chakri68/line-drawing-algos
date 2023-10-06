5b166d29e01e3b1ce3a60b22f3879004ca8292ad
chakri68 <chakridevireddy69@gmail.com>
2023-10-06 17:25:32 +0530

    draw_pixel function now takes color as input

--- utils.py
+++ utils.py
@@ -2,9 +2,10 @@ from typing import Tuple
 from turtle import Turtle
 
 
-def drawPixel(turtle: Turtle, pixel: Tuple[int, int]):
+def draw_pixel(turtle: Turtle, pixel: Tuple[int, int], color="black"):
     turtle.penup()
     turtle.goto(pixel)
+    turtle.color(color)
     turtle.pendown()
     turtle.dot(5)
     return turtle
