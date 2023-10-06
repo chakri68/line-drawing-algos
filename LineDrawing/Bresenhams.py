014b571d207aab0afb58643ac25a56dd543f9425
chakri68 <chakridevireddy69@gmail.com>
2023-10-06 18:58:03 +0530

    working Bressenhams algorithm

--- LineDrawing/Bresenhams.py
+++ LineDrawing/Bresenhams.py
@@ -8,22 +8,26 @@ class BresenhamLineDrawing(LineDrawingStrategy):
         x1, y1 = startPoint
         x2, y2 = endPoint
 
-        rise: int = y2 - y1
-        run: int = x2 - x1
+        rise: int = (y2 - y1)
+        run: int = (x2 - x1)
 
         slopeIsNegative: bool = (rise < 0) ^ (run < 0)
         points: List[Tuple[int, int]] = []
 
-        if (abs(run) > abs(rise)):
+        rise: int = abs(rise)
+        run: int = abs(run)
+
+        if (run > rise):
             # x increases faster than y
             if x1 > x2:
                 x1, x2 = x2, x1
                 y1, y2 = y2, y1
 
+            points.append((x1, y1))
             y = y1
             pk: int = 2 * rise - run  # magic formula
-            for x in range(x2, x2 + 1):
-                if pk <= 0:
+            for x in range(x1 + 1, x2 + 1):
+                if pk < 0:
                     # Select yk since dl < du
                     points.append((x, y))
                     pk += 2 * rise
@@ -40,10 +44,11 @@ class BresenhamLineDrawing(LineDrawingStrategy):
                 y1, y2 = y2, y1
                 x1, x2 = x2, x1
 
+            points.append((x1, y1))
             x = x1
             pk: int = 2 * run - rise  # magic formula
-            for y in range(y1, y2 + 1):
-                if pk <= 0:
+            for y in range(y1 + 1, y2 + 1):
+                if pk < 0:
                     points.append((x, y))
                     pk += 2 * run
                 else:
